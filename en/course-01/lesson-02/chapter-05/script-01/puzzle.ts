397f4688b92dd9a30b31c7a24f4d5d575a34b2a4
hh <zhfnjust@qq.com>
2021-09-17 10:29:47 +0800

    update chapter 05

--- en/course-01/lesson-02/chapter-05/script-01/puzzle.ts
+++ en/course-01/lesson-02/chapter-05/script-01/puzzle.ts
@@ -2,45 +2,22 @@ import {  bsv } from 'scryptlib';
 import { UTXO, wallet, Tx,  SignType } from './wallet';
 import { AbstractContract } from 'scryptlib/dist/contract';
 import {toRawTx } from './wutils';
-import { DotWallet } from './dotwallet';
-const WEB3_VERSION = '0.0.1';
 
 const FEE = 2000;
 
 export class web3 {
 
-
   static wallet: wallet;
-
-
-  static setWallet(wallet: wallet) {
-    web3.wallet = wallet;
-  }
-
-
-  static version() {
-    return WEB3_VERSION;
-  }
-
   
   static async buildDeployTx(contract: AbstractContract, amountInContract: number): Promise<Tx> {
 
-    let wallet = new DotWallet();
-
-    let changeAddress = '';
-    
-    let publicKey = '';
+    let changeAddress = '',  publicKey = '';
 
     const minAmount = amountInContract + FEE;
 
-    return wallet.listUnspent(minAmount, {
+    return web3.wallet.listUnspent(minAmount, {
       purpose: 'alice'
     }).then(async (utxos: UTXO[]) => {
-
-      if (utxos.length === 0) {
-        throw new Error('no utxos');
-      }
-
       
       const tx: Tx = {
         inputs: [],
@@ -55,20 +32,6 @@ export class web3 {
           utxo: utxos[0],
           script: '',
           sequence: 0
-        }
-      );
-
-      const changeAmount = utxos[0].satoshis - amountInContract - FEE;
-
-      if (changeAmount <= 0) {
-        throw new Error('fund is not enough');
-      }
-
-      const script = bsv.Script.buildPublicKeyHashOut(changeAddress).toHex();
-      tx.outputs.push(
-        {
-          script: script,
-          satoshis: changeAmount
         }
       );
 
--- en/course-01/lesson-02/chapter-05/script-01/solution.ts
+++ en/course-01/lesson-02/chapter-05/script-01/solution.ts
@@ -2,45 +2,22 @@ import {  bsv } from 'scryptlib';
 import { UTXO, wallet, Tx,  SignType } from './wallet';
 import { AbstractContract } from 'scryptlib/dist/contract';
 import {toRawTx } from './wutils';
-import { DotWallet } from './dotwallet';
-const WEB3_VERSION = '0.0.1';
 
 const FEE = 2000;
 
 export class web3 {
 
-
   static wallet: wallet;
-
-
-  static setWallet(wallet: wallet) {
-    web3.wallet = wallet;
-  }
-
-
-  static version() {
-    return WEB3_VERSION;
-  }
-
   
   static async buildDeployTx(contract: AbstractContract, amountInContract: number): Promise<Tx> {
 
-    let wallet = new DotWallet();
-
-    let changeAddress = '';
-    
-    let publicKey = '';
+    let changeAddress = '',  publicKey = '';
 
     const minAmount = amountInContract + FEE;
 
-    return wallet.listUnspent(minAmount, {
+    return web3.wallet.listUnspent(minAmount, {
       purpose: 'alice'
     }).then(async (utxos: UTXO[]) => {
-
-      if (utxos.length === 0) {
-        throw new Error('no utxos');
-      }
-
       
       const tx: Tx = {
         inputs: [],
@@ -61,20 +38,6 @@ export class web3 {
         }
       );
 
-      const changeAmount = utxos[0].satoshis - amountInContract - FEE;
-
-      if (changeAmount <= 0) {
-        throw new Error('fund is not enough');
-      }
-
-      const script = bsv.Script.buildPublicKeyHashOut(changeAddress).toHex();
-      tx.outputs.push(
-        {
-          script: script,
-          satoshis: changeAmount
-        }
-      );
-
 
       return tx;
     }).then((tx) => {
@@ -85,8 +48,10 @@ export class web3 {
         .toHex();
 
         tx.inputs[0].script = script;
+        
         return tx;
       })
     })
   }
+
 }
